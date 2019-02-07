37c63825e48fced69c1cd6acb65b948922619c68
Aymen Rebouh <aymenmse@gmail.com>
2019-02-07 12:03:18 +0900

    Create xcode project

---
+++ check-cpu-gpu/check-cpu-gpu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  check-cpu-gpu
+//
+//  Created by Rebouh Aymen on 07/02/2019.
+//  Copyright © 2019 Rebouh Aymen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
